3b7811922e9327b5ce5fc7906cbfe0e8ada3002b
TanyaFreebie <fjodorova.tanya@gmail.com>
2021-08-10 23:12:02 +0300

    Did some of controllers Need to check on couple solutions Next time: add diagnosis and admissions, add login conformation and redirection to menu main goal is to have only Login on class Main

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,13 +1,24 @@
 package com.company;
 
 
+import com.company.controlers.DoctorController;
+import com.company.controlers.DrugController;
 import com.company.controlers.PatientController;
+import com.company.controlers.UpdateDb;
+import com.company.objects.Doctor;
 
 public class Main {
     public static void main(String[] args){
 
-        PatientController.editPatient();
+///Need to update all conformation in Doctors and Patients controllers
+        //find a way to add a recursion if there was mistake and end recursion on command
 
+
+        //add admissions and UpdateDb - case menu - main menu leads to doctors/parients/drug/admission menu
+
+        //add users access to menu after login conformation
+
+        UpdateDb.mainMenu();
 
     }
 
