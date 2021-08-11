08569b2c19a8981eaf7ba8c74860ea34575cf968
TanyaFreebie <fjodorova.tanya@gmail.com>
2021-08-11 15:29:04 +0300

    Must rewrite user - logout option and redirecting to main menu

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,38 +1,11 @@
 package com.company;
 
-
-import com.company.controlers.DoctorController;
-import com.company.controlers.DrugController;
-import com.company.controlers.PatientController;
 import com.company.controlers.UpdateDb;
-import com.company.dbhelper.DbConnection;
-import com.company.objects.Doctor;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 public class Main {
     public static void main(String[] args){
-        PreparedStatement ps;
-
-        //find a way to add a recursion if there was mistake and end recursion on command
-
-
-        //add admissions and UpdateDb - case menu - main menu leads to doctors/parients/drug/admission menu
-
-        //add users access to menu after login conformation
-
-
-        //Hardcoding on filling up database
-//        try {
-//            ps = DbConnection.user().prepareStatement("INSERT INTO drugs(name, storage)" +
-//                    "VALUES ('vitamins', " + 200 + ")");
-//
-//            ps.execute();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+           UpdateDb.mainMenu();
     }
 
 }
